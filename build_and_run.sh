26fecb06a115d64b9c4a244d481d9c102759baae
dabannikov <DABannikov@sberbank.ru>
2021-10-23 17:09:36 +0300

    ready do templa

---
+++ build_and_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker-compose down
+
+./gen_certs.sh
+
+DOCKER_BUILDKIT=1 docker build . -f marketplace/Dockerfile -t marketplace --secret id=ca.key,src=ca.key
+DOCKER_BUILDKIT=1 docker build . -f recommendations/Dockerfile -t recommendations --secret id=ca.key,src=ca.key
+
+docker-compose up
